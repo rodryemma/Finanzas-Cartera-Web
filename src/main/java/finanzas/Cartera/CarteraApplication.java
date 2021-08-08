6ce7dc3751a90ac247bf07abb2576e0fa420fabe
rodryemma <rodymuba@gmail.com>
2021-08-08 17:53:36 -0300

    upload

---
+++ src/main/java/finanzas/Cartera/CarteraApplication.java
@@ -0,0 +1,13 @@
+package finanzas.Cartera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finanzas/Cartera/CarteraApplicationTests.java
@@ -0,0 +1,13 @@
+package finanzas.Cartera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
